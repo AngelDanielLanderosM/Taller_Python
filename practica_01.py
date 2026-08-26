eff39f07ec69a052ecebf296d99fc054d106452a
AngelDanielLanderosM <angel.add373@gmail.com>
2026-08-26 09:21:37 -0600

    Agregada practica 2 configuracion de laminador

---
+++ practica_01.py
@@ -0,0 +1,5 @@
+nombre_pieza = "Porta vasos"
+diametro_mm = 30
+pasa_inspeccion = True
+
+print("El reporte indica que la pieza {} mide {} mm. ¿Aprobada?: {}".format(nombre_pieza, diametro_mm, pasa_inspeccion))
---
+++ practica_02.py
@@ -0,0 +1,5 @@
+tipo_filamento = "PLA Pure"
+temperatura_cama = 60
+ventilador_activo = True
+
+print("Configuración de impresión: Filamento {}, cama a {} grados. Ventilador: {}".format(tipo_filamento, temperatura_cama, ventilador_activo))
