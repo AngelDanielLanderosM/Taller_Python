b34ff3e4bbd7f2fe79a08a5b1382e9c1cab376fb
AngelDanielLanderosM <angel.add373@gmail.com>
2026-08-26 09:25:02 -0600

    Agregada practica 3 especificacion de barreno

---
+++ practica_03.py
@@ -0,0 +1,5 @@
+tipo_rosca = "M8"
+profundidad_mm = 15
+rosca_pasante = False
+
+print("Especificación: Barreno tipo {} con {} mm de profundidad. ¿Atraviesa la pieza?: {}".format(tipo_rosca, profundidad_mm, rosca_pasante))
