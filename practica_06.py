44645e5485d8e614bc42875f890c5f1647c49266
AngelDanielLanderosM <angel.add373@gmail.com>
2026-08-27 11:38:02 -0600

    Agregada practica 6 Generador de etiquetas de Producción

---
+++ practica_06.py
@@ -0,0 +1,4 @@
+bandeja_impresion = ["Soporte Izquierdo", "Soporte Derecho", "Rejilla"]
+bandeja_impresion.append("Clip de Sujecion")
+for pieza in bandeja_impresion:
+    print("Generando etiqueta para: {}".format(pieza))
